0a9adb27bd49577bf5f5156f3bb83f5216db2b59
Dan Vy Ho <ho.danvy@gmail.com>
2025-03-26 14:51:52 -0400

    new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_26_181354) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_26_184111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.string "name"
+    t.float "quantity"
+    t.string "unit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
+  end
+
+  create_table "instruction_steps", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.integer "order"
+    t.text "text"
+    t.string "media_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_instruction_steps_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ingredients", "recipes"
+  add_foreign_key "instruction_steps", "recipes"
 end
